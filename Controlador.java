28d7f758860e017252ce9cbafcf9cd2d66a095d0
Esteban Donis <estebandonis10@gmail.com>
2021-11-02 16:27:30 -0600

    Modificacion controlador

--- Controlador.java
+++ Controlador.java
@@ -42,27 +42,37 @@ public class Controlador {
             else if (opcion == 2){
                 LocalDate fechaDeseada = vista.obtenerFecha();
                 ArrayList<Post> posts = sistem.getPosts();
+                int numero = 0;
                 for (int i = 0; i < posts.size(); i++){
                     Post post = posts.get(i);
                     if (fechaDeseada.equals(post.getFecha())){
                         vista.mostrarPost(post);
+                        numero += 1;
                     }
                 }
-                int num = vista.accion();
-                int num2 = vista.seleccionPost();
-                if (num == 1){
-                    sistem.agregarComentario(num2, vista.comentario());
+                if (numero > 0){
+                    int num = vista.accion();
+                    int num2 = vista.seleccionPost();
+                    if (num == 1){
+                        sistem.agregarComentario(num2, vista.comentario());
+                    }
+                    else if (num == 2){
+                        sistem.agregarLike(num2);
+                    }
+                    else if (num == 3){
+                        sistem.accion(num2);
+                    }
                 }
-                else if (num == 2){
-                    sistem.agregarLike(num2);
+                else {
+                    System.out.println("");
+                    System.out.println("No se encontraron ningun post en esa fecha en particular");
                 }
-                else if (num == 3){
-                    sistem.accion(num2);
-                }
+                
             }
             else if (opcion == 3){
                 String hashtag = vista.obtenerHashtags();
                 ArrayList<Post> posts = sistem.getPosts();
+                int numero = 0;
                 for (int i = 0; i < posts.size(); i++){
                     Post post = posts.get(i);
                     String hash = post.getHashtags();
@@ -71,20 +81,28 @@ public class Controlador {
                         hash = hashes[a];
                         if (hashtag.equalsIgnoreCase(hash)){
                             vista.mostrarPost(post);
+                            numero += 1;
                         }
                     }
                 }
-                int num = vista.accion();
-                int num2 = vista.seleccionPost();
-                if (num == 1){
-                    sistem.agregarComentario(num2, vista.comentario());
+                if (numero > 0){
+                    int num = vista.accion();
+                    int num2 = vista.seleccionPost();
+                    if (num == 1){
+                        sistem.agregarComentario(num2, vista.comentario());
+                    }   
+                    else if (num == 2){
+                        sistem.agregarLike(num2);
+                    }
+                    else if (num == 3){
+                        sistem.accion(num2);
+                    }
                 }
-                else if (num == 2){
-                    sistem.agregarLike(num2);
+                else{
+                    System.out.println("");
+                    System.out.println("No se encontro ningun post con ese hashtag en particular");
                 }
-                else if (num == 3){
-                    sistem.accion(num2);
-                }
+                
             }
             else if (opcion == 4){
                 System.exit(0);//Se sale del programa
